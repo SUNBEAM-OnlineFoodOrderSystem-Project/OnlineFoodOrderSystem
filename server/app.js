25a9e828ee901f49ed3187ca5fbadba162d921ba
Ajay Mule <muleajay002@gmail.com>
2025-08-05 12:57:12 +0530

    add login and registration API with JWT authentication

--- server/app.js
+++ server/app.js
@@ -4,10 +4,14 @@ require('dotenv').config();
 
 
 
+
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api/auth', authRoutes);
 
 
 
